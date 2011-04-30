eca5ecfe8403a4a573281ef67f8634d24f3367b6
SyncDB and sons <sons@SyncDB.org>
2011-05-01 00:41:12 +0200

    hmac implementation

---
+++ js/util.hmac.js
@@ -0,0 +1,87 @@
+var HMAC = Base.extend({
+    constructor : function(hashp) {
+	this.hashp = hashp;
+    },
+    get : function(key) {
+	return new this.HMAC(new this.hashp(), key);
+    },
+    HMAC : Base.extend({
+	constructor : function(hash, key) {
+	    var blen;
+	    this.hash = hash;
+	    hash.update("");
+	    blen = 64;
+	    hash.init();
+	    if (key.length > blen) {
+		hash.update(key);
+		key = hash.string_digest(key);
+		hash.init();
+	    }
+	    var a;
+
+	    if (key.length < blen) {
+		a = new Array(blen - key.length);
+		key += String.fromCharCode.apply(window, a);
+		console.log("sizeof key: %o, blen: %o, key: %o", key.length, blen, key);
+	    }
+	    a = new Array(blen);
+	    for (var i = 0; i < a.length; i++) {
+		a[i] = 0x5c;
+	    }
+	    console.log(">>>> %o, %o", String.fromCharCode.apply(window, a).length, key.length);
+	    //this.o_key_pad = this.xor(String.fromCharCode.apply(window, a), key);
+	    this.o_key_pad = this.XOR(key, 0x5c);
+	    for (var i = 0; i < a.length; i++) {
+		a[i] = 0x36;
+	    }
+	    //this.i_key_pad = this.xor(String.fromCharCode.apply(window, a), key);
+	    this.i_key_pad = this.XOR(key, 0x36);
+
+	    console.log(">> %o, %o", this.o_key_pad.length, this.i_key_pad.length);
+	},
+	XOR : function(s, i) {
+	    var a = new Array(s.length);
+	    for (var j = 0; j < a.length; j++)
+		a[j] = s.charCodeAt(j)^i;
+	    return String.fromCharCode.apply(window, a);
+	},
+	xor : function(one, two) {
+	    var three;
+
+	    if (one.length < two.length) {
+		var three = one;
+		two = one;
+		one = three;
+	    }
+
+	    three = new Array(one.length - two.length);
+
+	    console.log("o, t: %o, %o", one.length, two.length);
+	    for (var i = 0; i < (one.length - two.length); i++) {
+		console.log("DID IT %o %o", i, one.length - two.length);
+		two += String.fromCharCode(two.charCodeAt(i));
+		three[i] = two.charCodeAt(i);
+	    }
+	    two += String.fromCharCode.apply(window, three);
+
+	    for (var i = 0; i < one.length; i++) {
+		three[i] = one.charCodeAt(i)^two.charCodeAt(i%(two.length));
+	    }
+
+	    return String.fromCharCode.apply(window, three);
+
+	    return three;
+	},
+	hmac : function(s) {
+	    var inner;
+	    this.hash.update(this.i_key_pad + s)
+	    inner = this.hash.string_digest();
+	    this.hash.init();
+	    this.hash.update(this.o_key_pad + inner);
+	    inner = this.hash.hex_digest();
+	    //inner = this.hash.array_digest();
+	    this.hash.init();
+	    return inner;
+        }
+    })
+});
